5c1464fa5da06abefd5b924f1548950692b058d6
Cristiano <cristtiano.mendonca@gmail.com>
2023-07-10 13:38:23 -0300

    73. Exibindo números na Calculadora - Criação metodo eventoBotoes() - Criar método adicionarNumero(), inclui número na tela

--- assets/scripts/calculadora/CalculadoraControle.js
+++ assets/scripts/calculadora/CalculadoraControle.js
@@ -4,8 +4,47 @@ export default class CalculadoraControle {
     tela;
     constructor(tela = new Tela()) {
         this.tela = tela;
-        this.tela.conteudo = "1.5";
         new DataHora();
+        this.eventosBotoes();
+    }
+    eventosBotoes() {
+        document.querySelectorAll("#teclado button").forEach(elemento => {
+            elemento.addEventListener("click", (evento) => {
+                const target = evento.target;
+                switch (target.id) {
+                    case "zero":
+                    case "um":
+                    case "dois":
+                    case "tres":
+                    case "quatro":
+                    case "cinco":
+                    case "seis":
+                    case "sete":
+                    case "oito":
+                    case "nove":
+                        this.adicionarNumero(Number(target.dataset.valor));
+                        break;
+                    case "adicao":
+                    case "subtracao":
+                    case "divisao":
+                    case "multiplicao":
+                        break;
+                    case "ponto":
+                        break;
+                    case "limpar":
+                        break;
+                    case "desfazer":
+                        break;
+                    case "porcentagem":
+                        break;
+                    case "igual":
+                        break;
+                }
+            });
+        });
+    }
+    adicionarNumero(numero) {
+        this.tela.conteudo = numero.toString();
     }
 }
 //# sourceMappingURL=CalculadoraControle.js.map
--- assets/typescript/calculadora/CalculadoraControle.ts
+++ assets/typescript/calculadora/CalculadoraControle.ts
@@ -4,7 +4,65 @@ export default class CalculadoraControle {
     constructor(
         private tela = new Tela(),
     ){
-        this.tela.conteudo = "1.5";
         new DataHora()
+
+        this.eventosBotoes();
+    }
+
+    eventosBotoes(){
+        // Recupera todos os botões que estão na div de id teclado
+        document.querySelectorAll("#teclado button").forEach(elemento => {
+            elemento.addEventListener("click", (evento: Event) => {
+                const target = evento.target as HTMLButtonElement;
+
+                switch(target.id){
+                    case "zero":
+                    case "um":
+                    case "dois":
+                    case "tres":
+                    case "quatro":
+                    case "cinco":
+                    case "seis":
+                    case "sete":
+                    case "oito":
+                    case "nove":
+                        this.adicionarNumero(Number(target.dataset.valor));
+                    break;
+
+                    case "adicao":
+                    case "subtracao":
+                    case "divisao":
+                    case "multiplicao":
+
+                    break;
+
+                    case "ponto":
+
+                    break;
+
+                    case "limpar":
+
+                    break;
+
+                    case "desfazer":
+
+                    break;
+
+                    case "porcentagem":
+                    
+                    break;
+
+                    case "igual":
+
+                    break;
+
+                    
+                }
+            });
+        })
+    }
+
+    adicionarNumero(numero: number): void{
+        this.tela.conteudo = numero.toString();
     }
 }
